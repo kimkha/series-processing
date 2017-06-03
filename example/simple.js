80871390bd5748d19785ca0e26e92d5a7161764e
Nguyễn Kim Kha <nkimkha@gmail.com>
2017-06-03 12:14:15 +0700

    Refactor

--- example/simple.js
+++ example/simple.js
@@ -1,36 +1,78 @@
 const { TimeSeries, studyBuilder } = require('../lib');
 
 const series = new TimeSeries();
-series.map(studyBuilder.EMA('ema1', 'close', 2));
+series.map(studyBuilder.EMA('ema1', 'weight', 8));
 
 const data = [
   {
-    open: 232,
-    close: 232
-  },
-  {
-    open: 232,
-    close: 234
-  },
-  {
-    open: 234,
-    close: 237
-  },
-  {
-    open: 237,
-    close: 235
-  },
-  {
-    open: 235,
-    close: 236
-  },
-  {
-    open: 236,
-    close: 239
+    weight: 22.81
+  },{
+    weight: 23.09
+  },{
+    weight: 22.91
+  },{
+    weight: 23.23
+  },{
+    weight: 22.83
+  }, {
+    weight: 23.05
+  },{
+    weight: 23.02
+  }, {
+    weight: 23.29
+  },{
+    weight: 23.41
+  },{
+    weight: 23.49
+  },{
+    weight: 24.60
   }
 ];
+// const data = [
+//   {
+//     weight: 69.8
+//   },{
+//     weight: 70.3
+//   },{
+//     weight: 69.8
+//   },{
+//     weight: 69.4
+//   },{
+//     weight: 69.4
+//   },
+//   {
+//     weight: 68.9
+//   },{
+//     weight: 69.2
+//   },
+//   {
+//     weight: 70.1
+//   },{
+//     weight: 69.7
+//   },{
+//     weight: 69.1
+//   },{
+//     weight: 68.6
+//   },{
+//     weight: 69.1
+//   },{
+//     weight: 68.8
+//   },{
+//     weight: 69.8
+//   },{
+//     weight: 69.7
+//   },{
+//     weight: 69.1
+//   },{
+//     weight: 68.8
+//   },{
+//     weight: 68.6
+//   }, {
+//     weight: 68.7
+//   }
+// ];
 
 series.initData(data);
-series.appendData({ open: 239, close: 242 });
+// series.appendData({ open: 239, close: 242 });
 
 console.log(JSON.stringify(series.getDataSeries(), null, 2));
--- src/data-stream.js
+++ src/data-stream.js
@@ -1,24 +1,79 @@
-
 export default class DataStream {
-  constructor(data) {
+  constructor(data, options) {
     this.data = Object.assign([], data);
     this.length = this.data.length;
+
+    this.options = Object.assign({
+      // Default setting
+      timefield: 'timestamp',
+      timeformat: v => new Date(v),
+      timeblock: '1min',
+    }, options);
+
+    if (typeof this.options.timefield !== "string") {
+      throw Error(`options.timefield of DataStream must be a string.`);
+    }
+    if (typeof this.options.timeformat !== "function") {
+      throw Error(`options.timeformat of DataStream must be a function.`);
+    }
+
+    this.timeBlockInMs = 60000;
+    switch (this.options.timeblock) {
+      case '5min':
+        this.timeBlockInMs *= 5;
+        break;
+      case '15min':
+        this.timeBlockInMs *= 15;
+        break;
+      case '30min':
+        this.timeBlockInMs *= 30;
+        break;
+      case '1hr':
+        this.timeBlockInMs *= 60;
+        break;
+      case '2hr':
+        this.timeBlockInMs *= 120;
+        break;
+      case '3hr':
+        this.timeBlockInMs *= 180;
+        break;
+      case '12hr':
+        this.timeBlockInMs *= 720;
+        break;
+      case '1d':
+        this.timeBlockInMs *= 1440;
+        break;
+    }
   }
 
-  getData = () => this.data;
-  setData = (data) => {
+  getData() {
+    return this.data;
+  };
+  setData(data) {
     this.data = Object.assign([], data);
     this.length = this.data.length;
   };
-  push = (item) => {
+  push(item) {
     this.data.push(item);
     this.length = this.data.length;
   };
-  hasData = () => this.length > 0;
-  getLast = () => (this.length >= 1) ? this.data[this.length - 1] : null;
-  getPrevious = () => (this.length >= 2) ? this.data[this.length - 2] : null;
-  getLastSegment = (length) => {
+  hasData() {
+    return this.length > 0;
+  };
+  getLast() {
+    return (this.length >= 1) ? this.data[ this.length - 1 ] : null;
+  };
+  getPrevious() {
+    return (this.length >= 2) ? this.data[ this.length - 2 ] : null;
+  };
+  getLastSegment(length) {
     const segmentLength = Math.min(length, this.length);
     return this.data.slice(this.length - segmentLength);
+  };
+
+  calcDistanceWithLast() {
+    const lastTime = +this.options.timeformat(this.getLast()[this.options.timefield]);
+    const prevTime = +this.options.timeformat(this.getPrevious()[this.options.timefield]);
+    return Math.ceil((lastTime - prevTime) / this.timeBlockInMs); // deltaTime / timeBlock
   };
 }
